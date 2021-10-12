38e2a9bbcc8be21293f003cd44803d94eb00ddba
DanielPodbury <82479887+DanielPodbury@users.noreply.github.com>
2021-10-13 00:46:38 +1100

    AST output as json file source can be entered as string

--- comprende.py
+++ comprende.py
@@ -1,13 +1,16 @@
 import sys
-import time
 from lexer import *
 from token_parser import *
 
-def read_source(filename):
+def read_source(filename: str):
     source_code = ""
     with open(filename, 'r') as file:
         source_code = file.read()
     return source_code
+
+def write_to_json(string: str):
+    with open("./output/AST.json", 'w') as file:
+        file.write(string)
 
 def main(source_code):
 
@@ -22,16 +25,22 @@ def main(source_code):
     AST = TokenParser(tokens).parse()
     print(AST)
 
+    print("\nWriting to ./output/AST.json ...\n")
+    write_to_json(str(AST))
 
 
 if __name__ == "__main__":
     args = sys.argv
     if len(args) > 1:
-        source_code = read_source(args[1])
-    else:
-        print("Usage: py comprende.py <sourcefile.txt>")
-        print("Please specify sourcefile.\n")
-        print("Running default source file...\n")
+        print(type(args[1]))
+        if args[1][:1] == ">":  # use string as input if no file specified
+            source_code = args[1][1:]
+        else:
+            source_code = read_source(args[1])
 
-        source_code = read_source("./scripts/default_code.txt")
-    main(source_code)
+        main(source_code)
+
+    else:
+        print("Usage: py comprende.py <sourcefile.txt>  OR")
+        print("       py comprende.py '> source code...'")
+        print("Please specify sourcefile or source string\n")
